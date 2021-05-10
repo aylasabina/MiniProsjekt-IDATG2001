902ce68b390801f70bf68cc190c71d1389a5cd09
Ayla Sabina Saxell <aylass@stud.ntnu.no>
2021-05-10 18:16:44 +0200

    Implemented the main controller class

---
+++ src/main/java/no/ntnu/idatg2001/miniprosjekt/postalcode/controller/MainController.java
@@ -0,0 +1,78 @@
+package no.ntnu.idatg2001.miniprosjekt.postalcode.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import no.ntnu.idatg2001.miniprosjekt.postalcode.model.PostalCode;
+import no.ntnu.idatg2001.miniprosjekt.postalcode.model.PostalCodeRegister;
+
+import java.net.URL;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+
+/**
+ * The class for handling the main window.
+ *
+ * @author 10042
+ * @version 10.05.2021
+ */
+public class MainController implements Initializable {
+
+    private final PostalCodeRegister postalCodeRegister = PostalCodeRegister.getPostalCodeRegisterInstance();
+    private ObservableList<PostalCode> postalCodeObservableList;
+
+    @FXML
+    private TableView<PostalCode> tableView;
+    @FXML
+    private TableColumn<PostalCode, String> zipCodeColumn;
+    @FXML
+    private TableColumn<PostalCode, String> cityColumn;
+    @FXML
+    private TableColumn<PostalCode, String> municipalityColumn;
+    @FXML
+    private TextField searchField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        zipCodeColumn.setCellValueFactory(new PropertyValueFactory<>("zipCode"));
+        cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
+        municipalityColumn.setCellValueFactory(new PropertyValueFactory<>("municipality"));
+
+        tableView.setItems(getPostalCodeListWrapper());
+    }
+
+    /**
+     * Get the list of postal codes wrapped in an observable list.
+     *
+     * @return the observable list of the postal codes.
+     */
+    private ObservableList<PostalCode> getPostalCodeListWrapper() {
+        postalCodeObservableList = FXCollections.observableArrayList(postalCodeRegister.getPostalCodes());
+        return postalCodeObservableList;
+    }
+
+    @FXML
+    public void textChanged(KeyEvent keyEvent) {
+        HashSet<PostalCode> results = postalCodeRegister.searchPostalCodes(searchField.getText());
+        postalCodeObservableList.setAll(results);
+    }
+
+    @FXML
+    public void infoButtonClicked(ActionEvent actionEvent) {
+        Alert info = new Alert(Alert.AlertType.INFORMATION);
+        info.setHeaderText("Information");
+        info.setContentText("You can search for a postal code by:\n" +
+                "\t1. The zip code (format 0000)\n" +
+                "\t2. The city name\n" +
+                "The table will update as you are searching.");
+        info.showAndWait();
+    }
+}
